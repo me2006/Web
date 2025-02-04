2697db24ac3acb4dce22bff30c514049a13e6745
Michael <91761269+natural-stupidity@users.noreply.github.com>
2025-02-03 22:56:29 -0500

    Added fanart. Added base infobox. More styling fixes
    Signed-off-by: Michael <91761269+natural-stupidity@users.noreply.github.com>

---
+++ src/components/Wiki/InfoBox/index.tsx
@@ -0,0 +1,93 @@
+import clsx from "clsx";
+import styles from "./style.module.css";
+
+type InfoBoxItem = {
+  title: string;
+  imgUrl: string;
+  imgAlt: string;
+  caption: string;
+  categories: object;
+};
+
+function InfoBoxDataGroup({categoryTitle, categoryChildren}){
+  console.log(categoryTitle + " - " + categoryChildren + " - " + typeof categoryChildren)
+  console.log(categoryChildren )
+  return (
+    <>
+      <div className={styles.separator} />
+      {
+      !(categoryChildren instanceof Object )?
+      <div className={styles.infoboxData}>
+        <InfoboxDataRow title={categoryTitle} data={[categoryChildren]}/>
+      </div>
+      :
+      <div className={styles.infoboxGroup}>
+        <div className={styles.heading}>
+          <h3>{categoryTitle}</h3>
+        </div>
+        <div className={styles.infoboxData}>
+          {Object.keys(categoryChildren).map((key, idx) => (
+            <InfoboxDataRow key={key+idx} title={key} data={[categoryChildren[key]]}/>
+          ))}
+        </div>
+      </div>
+      }
+    </>
+  )
+}
+
+function InfoboxDataRow({title, data}) {
+  return (
+    <div className={styles.infoboxDataRow}>
+      <p className={styles.dataHeading}>{title}</p>
+      <ul className={styles.dataContent}>
+        {data.map((item, idx) => (
+          <li key={item+idx}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+/**
+ * Creates a wikipedia style info box
+ * 
+ * Note: For categories with no title / header, use a string:
+ * <categoryTitle>: <string>
+ * Otherwise, use an object
+ * <categoryTitle>: { <key1> : <value1>, <key2> : <value2> ...}
+ * 
+ * Syntax:
+ * 
+ *  <InfoBox 
+      title={""}
+      imgUrl={""}
+      imgAlt={""}
+      caption={""}
+      categories={{
+        <categoryTitle>: <categoryChildren (string | object)>,
+        ...
+      }
+    />
+ */
+export default function InfoBox({title, imgUrl, imgAlt, caption, categories }: InfoBoxItem) {
+  return (
+    <div className="ytvContainer orange">
+      <div className={styles.titleHeadingContainer}>
+        <h2 className={styles.titleHeading}>{title}</h2>
+      </div>
+      <div className={clsx(styles.infobox, "ytvContainerInner green")}>
+        <div> 
+          <img src={imgUrl} className={styles.infoboxImg} alt={imgAlt}/>
+          <p className={styles.infoboxImgCaption}>{caption}</p> 
+        </div>
+        {
+          Object.keys(categories).map((key, idx) => (
+            <InfoBoxDataGroup key={key+idx} categoryTitle={key} categoryChildren={categories[key]} />
+          ))
+        }
+      </div>
+    </div>
+  );
+}
+
--- src/pages/fanart/index.tsx
+++ src/pages/fanart/index.tsx
@@ -3,6 +3,8 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import Layout from '@theme/Layout';
 import styles from './index.module.css';
 import Carousel from '../../components/Carousel';
+import { faBluesky, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 type GalleryItem = {
   urls: string[];
@@ -10,33 +12,75 @@ type GalleryItem = {
   title: string;
   hoverEffect: boolean;
   isLink: boolean;
+  socialLinks: object;
 };
 
 const ArtList: GalleryItem[] = [
-  
+  {
+    urls: ["/img/fan_art/bkells/bkells.png"],
+    alt: `"Dr. Frantic pumpkin" created by bkells`,
+    title: `"Dr. Frantic pumpkin" created by bkells`,
+    hoverEffect: false,
+    isLink: false,
+    socialLinks: {}
+  },
+  {
+    urls: ["/img/fan_art/cyby/cyby1.png", "/img/fan_art/cyby/cyby2.png", "/img/fan_art/cyby/cyby3.png"],
+    alt: `"3D Sitekick "created by cyby`,
+    title: `"3D Sitekick" created by cyby`,
+    hoverEffect: false,
+    isLink: false,
+    socialLinks: {}
+  },
+  {
+    urls: ["/img/fan_art/puppy/puppy1.png", "/img/fan_art/puppy/puppy2.png"],
+    alt: `"Sitekick drawing" by puppy`,
+    title: `"Sitekick drawings" by puppy`,
+    hoverEffect: false,
+    isLink: false,
+    socialLinks: {
+      "https://www.instagram.com/idogmini/": <FontAwesomeIcon icon={faInstagram}/>,
+      "https://bsky.app/profile/idogmini.bsky.social": <FontAwesomeIcon icon={faBluesky}/>
+    }
+  },
+  {
+    urls: ["https://www.youtube.com/embed/7oe_WZpAVP0?si=aZ9MW7RaVx4bskC-"],
+    alt: `"Sitekick Remastered Tribute?! (December 2023)" by [GM] me2006`,
+    title: `"Sitekick Remastered Tribute?! (December 2023)" by [GM] me2006`,
+    hoverEffect: false,
+    isLink: true,
+    socialLinks: {}
+  }
 ];
 
-function GalleryItem({ urls, alt, title, hoverEffect, isLink }: GalleryItem) {
+function GalleryItem({ urls, alt, title, hoverEffect, isLink, socialLinks }: GalleryItem) {
   return (
 
     <div className={styles.galleryItem}>
       <div className={styles.galleryItemInner}>
         {!hoverEffect && urls.length > 1 ?
           <div>
-            <Carousel autoPlay={false} pictures={urls}/>
+            <Carousel autoPlay={false} pictures={urls} />
           </div>
           :
           hoverEffect ?
           <div className={styles.hoverImg}>
-            <img className={styles.img1} src={useBaseUrl(urls[0])}/>
+              <img className={styles.img1} src={useBaseUrl(urls[0])} />
             <img className={styles.img2} src={useBaseUrl(urls[1])} /> 
           </div>
           : isLink ?
-          <iframe width="100%" height="350px" src={urls[0]} title="YouTube video player" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+              <iframe width="100%" height="350px" src={urls[0]} title="YouTube video player" allowFullScreen></iframe>
           :
           <img src={useBaseUrl(urls[0])} alt={alt} />
         }
         <p className={styles.galleryTitle}>{title}</p>
+        <div className={styles.socialLinks}>
+        {
+          Object.keys(socialLinks).map((key) => (
+            <a href={key}>{socialLinks[key]}</a>
+          ))
+        }
+        </div>
       </div>
     </div>
   );
@@ -46,14 +90,12 @@ export default function FanArt(): ReactNode {
   //const colours = ["#9c102b", "#FAF9F6", "#8397b0", "#f0ead6", "#76736e"];
   //const bgColour = colours[Math.floor(Math.random() * colours.length)];
   const bgNum = Math.floor(Math.random() * 4) + 1;
-  const bgLoc = `/img/fan_art/backgrounds/background${bgNum}.svg`
+  const bgLoc = `/img/fan_art/backgrounds/background${bgNum}.png`
 
   return (
     <Layout title={`Fan Art`} description="Clickity-click, it's Sitekick!">
       <main style={{
         background: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${bgLoc}) repeat`,
-        backgroundSize: "contain",
-        color: "#000",
       }}>
         <div className={styles.fanArtContainer}>
           <div className={styles.pageTitleOuter}>
