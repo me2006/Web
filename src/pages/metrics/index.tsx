ba35d98ad22fd2a0166ad72091ac414b2139bf7d
Michael <91761269+natural-stupidity@users.noreply.github.com>
2025-02-04 21:14:02 -0500

    Stylized metrics page
    Signed-off-by: Michael <91761269+natural-stupidity@users.noreply.github.com>

--- src/pages/metrics/index.tsx
+++ src/pages/metrics/index.tsx
@@ -1,5 +1,9 @@
 import Layout from "@theme/Layout";
 import { useState, useEffect } from 'react';
+import styles from './index.module.css';
+import clsx from "clsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightDots, faCalendarCheck, faUser, faUserClock, faUserPlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function Status() {
   const [data, setData] = useState({});
@@ -7,9 +11,9 @@ export default function Status() {
     fetch("https://game.sitekickremastered.com/metrics/generic?q=online_players,daily_online_players,daily_registrations,total_players,total_chips")
       .then((res) => { return res.json(); })
       .then((data) => { setData(data); })
-      .catch((err) => {console.log(err); setData({}); });
+      .catch((err) => { console.log(err); setData({}); });
   }
-  
+
   useEffect(() => {
     getData();
     const interval = setInterval(() => getData(), 30 * 1000);
@@ -18,41 +22,64 @@ export default function Status() {
 
   return (
     <Layout title={`Metrics`} description="Clickity-click, it's Sitekick!">
-      { (Object.keys(data).length) ? 
-      <div className="grid cards">
-        <ul>
-          <li>
-            <p><strong>Players Online</strong></p>
-            <hr />
-              <p>There are <span>{data["online_players"]}</span> players online right now!</p>
-          </li>
-          <li>
-            <p><strong>Total Members</strong></p>
-            <hr />
-              <p><span>{data["total_players"]}</span> members have registered since November 29, 2020.</p>
-          </li>
-          <li>
-            <p><strong>New Players</strong></p>
-            <hr />
-              <p>Today <span>{data["daily_registrations"]}</span> new players registered.</p>
-          </li>
-          <li>
-            <p><strong>Played Today</strong></p>
-            <hr />
-              <p><span>{data["daily_online_players"]}</span> players logged in today.</p>
-          </li>
-          <li>
-            <p><strong>Total Chips</strong></p>
-            <hr />
-              <p>There are currently <span>{data["total_chips"]}</span> chips in circulation.</p>
-          </li>
-        </ul>
+      <div className={styles.metricsContainer}>
+        <h1 className={styles.title}>Metrics</h1>
+        {(Object.keys(data).length) ?
+          <>
+
+            <p>This page is updated every 30 seconds!</p>
+            <div className="row">
+              <div className={clsx("col", styles.metricsCard)}>
+                <h2>Players Online</h2>
+                <FontAwesomeIcon icon={faUserClock} size="6x" />
+                <hr />
+                {
+                  data["online_players"] == 0 ?
+                  <p>There are no players online right now.</p> :
+                  <p>There are {data["online_players"]} players online right now!</p>
+                }
+              </div>
+              <div className={clsx("col", styles.metricsCard)}>
+                <h2>Total Members</h2>
+                <FontAwesomeIcon icon={faUser} size="6x" />
+                <hr />
+                <p>{data["total_players"]} members have registered since November 29, 2020.</p>
+              </div>
+              <div className={clsx("col", styles.metricsCard)}>
+                <h2>New Players</h2>
+                <FontAwesomeIcon icon={faUserPlus} size="6x" />
+                <hr />
+                {
+                  data["daily_registrations"] == 0 ?
+                  <p>No new players have registered today.</p> :
+                  <p>Today {data["daily_registrations"]} new players registered.</p>
+                }
+              </div>
+              <div className={clsx("col", styles.metricsCard)}>
+                <h2>Played Today</h2>
+                <FontAwesomeIcon icon={faCalendarCheck} size="6x" />
+                <hr />
+                {
+                  data["daily_online_players"] == 0 ?
+                  <p>Noone has logged in today.</p> :
+                  <p>{data["daily_online_players"]} players have logged in today.</p>
+                }
+              </div>
+              <div className={clsx("col", styles.metricsCard)}>
+                <h2>Total chips</h2>
+                <FontAwesomeIcon icon={faArrowUpRightDots} size="6x" />
+                <hr />
+                <p>There are currently <span>{data["total_chips"]}</span> chips in circulation.</p>
+              </div>
+            </div>
+          </>
+          :
+          <>
+            <p>Failed to get metrics from the server. Please check <a href="/status">sitekickremastered.com/status</a> to see our server status</p>
+            <img src="/img/404.jpg" style={{maxHeight: "55vh"}}/>
+          </>
+        }
       </div>
-      :
-      <div>
-        Failed to get metrics!
-      </div>
-      }
     </Layout>
   );
 }
