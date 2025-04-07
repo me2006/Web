58d7c3b5611a64dd58f2a2a4e5e827899e30345f
Michael <91761269+natural-stupidity@users.noreply.github.com>
2025-04-06 23:05:25 -0400

    Reworked mod panel login. Added placeholders for tabs

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,23 +1,59 @@
+import { useState, createContext } from 'react';
 import Login from '@site/src/components/Admin/Login';
 import Panel from '@site/src/components/Admin/Panel';
-import { type ReactNode, useState } from 'react';
 
 import styles from "./index.module.css";
 
-export default function Admin(): ReactNode {
-  const [user, setUser] = useState({});
+export const UserContext = createContext(null);
+
+export default function Admin() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  function login(email: string, password: string) {
+    const data = {
+      email: email,
+      password: password
+    };
+
+    //security.fileuri.strict_origin_policy
+    fetch("http://localhost:8080/mod_panel_login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Access-Control-Allow-Headers": "Content-Type"
+      },
+
+      body: "json=" + encodeURIComponent(JSON.stringify(data))
+    }).then(res => {
+      if (!res.ok) {
+        alert("Incorrect username / password");
+        throw new Error("Incorrect username / password");
+      }
+      return res.json()
+    }).then(resData => {
+      // console.info(resData);
+      localStorage.setItem("userInfo", JSON.stringify(resData));
+      setLoggedIn(true);
+    }).catch(error => {
+      console.error("Error:", error);
+    });
+  }
+
+  function logout() {
+    localStorage.removeItem("userInfo");
+    setLoggedIn(false);
+  }
+
+  function getCurrentUser() {
+    const userStr = localStorage.getItem("userInfo");
+    return (userStr) ? JSON.parse(userStr) : null;
+  }
 
   return (
     <main className={styles.mainContainer}>
-      { !isEmpty(user) ? 
-        <Panel user={user} /> :
-        <Login setUser={setUser} />
-      }
-      
+      <UserContext.Provider value={{ login: login, logout: logout, getCurrentUser: getCurrentUser }}>
+        { !loggedIn ? <Login /> : <Panel /> }
+      </UserContext.Provider>
     </main>
   );
-}
-
-function isEmpty(obj) {
-  return Object.keys(obj).length === 0;
 }
